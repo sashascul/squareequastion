cd5d14788c05f1bf8f163a3c7348963e2de025d1
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-18 22:26:14 +0600

    Add files via upload

---
+++ Безымянный15.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+  
+int main()
+{
+    double a,b,c; //a*x*x + b*x + c = 0
+    double x1,x2; // roots
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a == 0 && b == 0 && c == 0){
+        printf("INFINITY ROOTS");
+        return 0; // THE END :)
+    }
+     
+    if(a!=0){
+        if(b*b-4*a*c > 0){
+            printf("2 \n");
+            x1 = (-b-sqrt(b*b-4*a*c))/(2*a);
+            x2 = (-b+sqrt(b*b-4*a*c))/(2*a);
+                if(x1 != 0)
+                    printf("%lf\n", x1);
+            	else
+                    printf("0\n");
+            if(x2 != 0)
+                printf("%lf", x2);
+            else
+            	printf("0");  
+        }
+        else
+            if(b*b-4*a*c == 0){
+                printf("1");
+                x1 = -b/(2*a);
+                if(x1 != 0)
+                    printf("%lf", x1);
+                else
+                    printf("0");
+            }
+            else
+                if(b*b-4*a*c < 0)
+                    printf("0");
+    }
+    else
+        if(b!=0){
+            printf("1");
+            if(c!=0)
+                x1 = -c/b;
+            else
+            printf("0");
+        }
+        else
+            if(c!=0)
+                printf("0");
+    return 0;
+}
