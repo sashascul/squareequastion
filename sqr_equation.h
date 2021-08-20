bb24ccbc73dddb7712199b551f13d53b58dffa99
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-20 23:00:08 +0600

    Add files via upload

---
+++ sqr_equation.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+void line_equation(double a, double b, double c, double *pX11, int *pRoots1);
+
+int square_equation(double a, double b, double c, double *pX1, double *pX2, int *pRoots);
+
+void forSwitch(int roots, double x1, double x2);
+
+void determineNumber(double a, double b, double c, double x1, double x2, int roots);
+
+enum wordNumbers {
+	ZERO = 0, 
+	ONE = 1, 
+	TWO = 2, 
+	MANYROOTS = 8
+};
+
+
