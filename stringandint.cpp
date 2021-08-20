bd32da1465bd73975e9cd78e8bb921ba9256d2ae
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-20 12:39:20 +0600

    Update stringandint.cpp

--- stringandint.cpp
+++ stringandint.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main() {
 	
-	string a, b, c;
+	char a[10], b[10], c[10];
 	int counter = 0, a1 = 0, b1 = 0, c1 = 0;
-	cin >> a >> b >> c;
+	scanf("%s %s %s", a, b, c);
 	
-	for (int i = 0; i < a.length(); i++) {
+	for (int i = 0; i < 10; i++) {
 		if ((int)a[i] < 48 || (int)a[i] > 57) {
 			counter++;
 		}
 	}
 	
-	for (int i = 0; i < b.length(); i++) {
+	for (int i = 0; i < 10; i++) {
 		if ((int)b[i] < 48 || (int)b[i] > 57) {
 			counter++;
 		}
 	}
 	
-	for (int i = 0; i < c.length(); i++) {
+	for (int i = 0; i < 10; i++) {
 		if ((int)c[i] < 48 || (int)c[i] > 57) {
 			counter++;
 		}
@@ -32,6 +32,6 @@ int main() {
 	}
 	
 	else
-		cout << "ERROR";
+		printf("ERROR");
 	
 }
