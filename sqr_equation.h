0b259683ad44a1401b1330b9ae84c3b8960576aa
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-26 23:09:42 +0600

    Update sqr_equation.h

--- sqr_equation.h
+++ sqr_equation.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 int comparisonOfNumber(double *x, double *y);
 
@@ -12,6 +13,9 @@ void forSwitch(int roots, double x1, double x2);
 void compareNumber(double a, double b, double c, const double EPS);
 
 void determineNumber(double a, double b, double c, double x1, double x2, int roots);
+
+int CV (double a, double b, double c, double *pX1, double *pX2);
+
 
 enum wordNumbers {
 	ZERO = 0, 
