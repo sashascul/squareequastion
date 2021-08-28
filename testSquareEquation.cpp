28b81495a8ac58a903f74de5bfbcfa5a4626a482
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-28 22:03:36 +0600

    Update testSquareEquation.cpp

--- testSquareEquation.cpp
+++ testSquareEquation.cpp
@@ -1,61 +1,113 @@
 #include "square_equation.h"
+#include "testSquareEquation.cpp"
 
-enum wordNumbers {
-	ZERO = 0, 
-	ONE = 1, 
-	TWO = 2, 
-	MANYROOTS = -1
-};
 
-int testSquare (double a, double b, double c, double True_x1, double True_x2, int True_roots) {
-	
-	double x1 = NAN, x2 = NAN;
+int main()
+{
+    double a = NAN, b = NAN, c = NAN, x1 = 0, x2 = 0;
 	int roots = 0;
-	
+		//unitTest();
+	determineNumber(&a, &b, &c);
 	square_equation(a, b, c, &x1, &x2, &roots);
-	
-	if (roots != True_roots)
-		return 0;
-	if (x1 != True_x1) 
-		return 0;
-	if (x2 != True_x2)
-		return 0;
-	
-	return 1;
-	
+	forSwitch(roots, x1, x2);
 }
 
-int unitTest() {
+int cON(double x) { // comparisonOfNumber
+
+    assert (isfinite(x));
+
+	const double EPS = 0.000001;
+	if (fabs(x) < EPS)
+		return 1;
+	return 0;
+}
+
+void line_equation(double a, double b, double c, double *pX11, int *pRoots1) { 
+
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    assert (pX11 != 0);
+    assert (pRoots1 != 0);
+   
+		if (cON(b) == 0){
+        	*pRoots1 = 1;
+        	if (cON(c) == 0)
+            	*pX11 = -c / b;
+        	else
+            	*pX11 = 0;
+        }
+        else 
+            if (cON(c) == 0)
+                *pRoots1 = 0;	
+}
+
+int square_equation(double a, double b, double c, double *pX1, double *pX2, int *pRoots) {	
+
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    assert (pX1 != 0);
+    assert (pX2 != 0);
+    assert (pX1 != pX2);
+
+	if (cON(a) == 1 && cON(b) == 1 && cON(c) == 1){
+        *pRoots = MANYROOTS;
+        return 0;
+    } 
+    
+    if (cON(a) == 0){
+    	double D = b * b - 4 * a * c, sqrtD = sqrt(D), twoA = 2 * a;
+        if (D > 0){
+        	*pRoots = TWO;
+            *pX1 = (-b - sqrtD)/twoA;
+            *pX2 = (-b + sqrtD)/twoA;
+        }
+        else
+            if (cON(D) == 1){
+            	*pRoots = ONE;
+                *pX1 = -b/twoA;
+            }
+            else
+                if (D < 0)
+                    *pRoots = ZERO;
+    }
+    else {	
+    	line_equation(a, b, c, pX1, pRoots);
+	}
+}
+
+void forSwitch(int roots, double x1, double x2) {
 	
-	int error = 0, roots = 0;
-	double x1 = NAN, x2 = NAN;
+    assert (isfinite(x1));
+    assert (isfinite(x2));
+
+		switch(roots) {
+		case ZERO:
+			printf("NO ROOTS\n");
+			break;
+		case ONE: 
+			printf("ROOT: 1\n%lf\n", x1);
+			break;
+		case TWO:
+			printf("ROOTS: 2\n%lf\n%lf\n", x1, x2);
+			break;
+		case MANYROOTS:
+			printf("INFINITY ROOTS\n");
+			break;
+	}
+}
+
+
+void determineNumber(double *a, double *b, double *c) {
 	
-	error = error + testSquare(1, -3, 2, 2, 1, 2);
-		square_equation(1, -3, 2, &x1, &x2, &roots);
-	printf("x^2 - 3x + 2 = 0\nResults of your equation:\nROOTS:%d\n%lf\n%lf\t", roots, x1, x2);
-		if (roots == TWO && x1 == 1 && x2 == 2)
-			printf("Good test\n");
-	x1 = NAN; x2 = NAN;
-	error = error + testSquare(0, -1, 6, 6, NAN, 1);
-		square_equation(0, -1, 6, &x1, &x2, &roots);
-	printf("-x + 6 = 0\nResults of your equation:\nROOTS:%d\n%lf\n%lf\t", roots, x1, x2);
-		if (roots == ONE && x1 == 6)
-			printf("Good test\n");
-	x1 = NAN; x2 = NAN;
-	error = error + testSquare(1, 1, 1, NAN, NAN, 0);
-		square_equation(1, 1, 1, &x1, &x2, &roots);
-	printf("x^2 + x + 1 = 0\nResults of your equation:\nROOTS:%d\n%lf\n%lf\t", roots, x1, x2);
-		if (roots == ZERO)
-			printf("Good test\n");
-	x1 = NAN; x2 = NAN;
-	error = error + testSquare(0, 0, 0, NAN, NAN, MANYROOTS);
-		square_equation(0, 0, 0, &x1, &x2, &roots);
-	printf("0x^2 + 0x + 0 = 0\nResults of your equation:\nROOTS:%d\n%lf\n%lf\t", roots, x1, x2);
-		if (roots == MANYROOTS)
-			printf("Good test\n");
+    printf("Hello! This program can decide a square equation. Input parametrs a, b, c in a space: ");
+    scanf("%lf %lf %lf", a, b, c);
 	
-	printf("Error tests: %d\n", error);
-	
-	return error;
-	
+    if (isnan(*a) == 0 && isnan(*b) == 0 && isnan(*c) == 0) {
+		return;
+	}	
+	else 
+		printf("ERROR: Not a numbers\n");	
+		exit(-1);
 }
