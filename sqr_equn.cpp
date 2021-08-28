518fc4a353fc2249dfa82c4937c11175eab09b55
sashascul <60592559+sashascul@users.noreply.github.com>
2021-08-28 18:18:25 +0600

    Update sqr_equn.cpp

--- sqr_equn.cpp
+++ sqr_equn.cpp
@@ -1,109 +1,19 @@
-#include "square_equation.h"
-#include "testSquareEquation.cpp"
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
 
-int main()
-{
-    double a = NAN, b = NAN, c = NAN, x1 = 0, x2 = 0;
-	int roots = 0;
-	determineNumber(a, b, c, x1, x2, roots);
+int comparisonOfNumber(double *x, double *y);
 
-}
+void line_equation(double a, double b, double c, double *pX11, int *pRoots1);
 
-int cON(double x) { // comparisonOfNumber
+int square_equation(double a, double b, double c, double *pX1, double *pX2, int *pRoots);
 
-    assert (isfinite(x));
+void forSwitch(int roots, double x1, double x2);
 
-	const double EPS = 0.000001;
-	if (fabs(x) < EPS)
-		return 1;
-	return 0;
-}
+void compareNumber(double a, double b, double c, const double EPS);
 
-void line_equation(double a, double b, double c, double *pX11, int *pRoots1) { 
+void determineNumber(double *a, double *b, double *c);
 
-    assert (isfinite(a));
-    assert (isfinite(b));
-    assert (isfinite(c));
-    assert (pX11 != 0);
-    assert (pRoots1 != 0);
-   
-		if (cON(b) == 0){
-        	*pRoots1 = 1;
-        	if (cON(c) == 0)
-            	*pX11 = -c / b;
-        	else
-            	*pX11 = 0;
-        }
-        else 
-            if (cON(c) == 0)
-                *pRoots1 = 0;	
-}
+int testSquare(double a, double b, double c, double True_x1, double True_x2, int True_roots);
 
-int square_equation(double a, double b, double c, double *pX1, double *pX2, int *pRoots) {	
-
-    assert (isfinite(a));
-    assert (isfinite(b));
-    assert (isfinite(c));
-    assert (pX1 != 0);
-    assert (pX2 != 0);
-    assert (pX1 != pX2);
-
-	if (cON(a) == 1 && cON(b) == 1 && cON(c) == 1){
-        *pRoots = MANYROOTS;
-        return 0;
-    } 
-    if (cON(a) == 0){
-    	double D = b * b - 4 * a * c, sqrtD = sqrt(D), twoA = 2 * a;
-        if (D > 0){
-        	*pRoots = TWO;
-            *pX1 = (-b - sqrtD)/twoA;
-            *pX2 = (-b + sqrtD)/twoA;
-        }
-        else
-            if (cON(D) == 1){
-            	*pRoots = ONE;
-                *pX1 = -b/twoA;
-            }
-            else
-                if (D < 0)
-                    *pRoots = ZERO;
-    }
-    else {	
-    	line_equation(a, b, c, pX1, pRoots);
-	}
-}
-
-void forSwitch(int roots, double x1, double x2) {
-	
-    assert (isfinite(x1));
-    assert (isfinite(x2));
-
-		switch(roots) {
-		case ZERO:
-			printf("NO ROOTS");
-			break;
-		case ONE: 
-			printf("ROOT: 1\n%lf", x1);
-			break;
-		case TWO:
-			printf("ROOTS: 2\n%lf\n%lf", x1, x2);
-			break;
-		case MANYROOTS:
-			printf("INFINITY ROOTS");
-			break;
-	}
-}
-
-
-void determineNumber(double a, double b, double c, double x1, double x2, int roots) {
-	
-    printf("Hello! This program can decide a square equation. Input parametrs a, b, c in a space: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-	
-    if (isnan(a) == 0 && isnan(b) == 0 && isnan(c) == 0) {
-    	square_equation(a, b, c, &x1, &x2, &roots);
-    	forSwitch(roots, x1, x2);
-	}	
-	else 
-		printf("ERROR: Not a numbers");		
-}
+int unitTest();
